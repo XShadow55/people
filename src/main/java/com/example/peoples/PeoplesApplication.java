49007496276d8e1eaef9a6892bc276e69ab847dd
Свинухов константин <lopata216@yandex.ru>
2023-10-31 20:37:46 +0300

    Работа с коллекциями

---
+++ src/main/java/com/example/peoples/PeoplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.peoples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplesApplication.class, args);
+	}
+
+}
